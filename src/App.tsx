ce0c59e4cb7ff1add8db2bd06faf5a1177a584ed
Rupe88 <chyrupesh828@gmail.com>
2024-07-04 21:47:18 +0545

    react-redux setup configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 const App = () => {
   return (
-    <div>hello world</div>
-  )
-}
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>THis is Home page</h1>} />
+            <Route path="/register" element={<h1>this is register page</h1>} />
+            <Route path="/login" element={<h1>this is login page</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+};
 
 export default App;
